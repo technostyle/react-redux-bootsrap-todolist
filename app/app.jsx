c179b87542fb5fa33a67fa03e611bd940845d5c9
danila <dm.perlov@jet.su>
2020-07-23 23:12:37 +0300

    removed extra code

--- app/app.jsx
+++ app/app.jsx
@@ -5,7 +5,6 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import store from "./store";
 import { DailyTodos } from "./containers/daily-todos";
 
-// console.log(window.ipcRenderer)
 ReactDOM.render(
   <Provider store={store}>
     <DailyTodos />
