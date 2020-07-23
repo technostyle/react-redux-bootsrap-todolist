f92fc250e6c54de9a9f14e401db1a86ca55d9502
danila <dm.perlov@jet.su>
2020-07-23 23:15:59 +0300

    date instead of day

--- app/utils/index.js
+++ app/utils/index.js
@@ -3,9 +3,9 @@ import { get } from "lodash";
 export const prop = (path, defaultValue) => obj => get(obj, path, defaultValue);
 
 const addOToDigit = value => (Number(value) < 10 ? `0${value}` : value);
-export const formatDate = id => {
+export const  formatDate = id => {
   const date = new Date(id);
-  return `${addOToDigit(date.getDay())}/${addOToDigit(
+  return `${addOToDigit(date.getDate())}/${addOToDigit(
     date.getMonth() + 1
   )}/${date.getFullYear()}`;
 };
