553e8802bf573516a83b94a5cea01064b297057f
danila <dm.perlov@jet.su>
2020-07-25 08:20:01 +0300

    shuffling todolist animation

--- app/containers/daily-todos/list/list.jsx
+++ app/containers/daily-todos/list/list.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ListGroup from "react-bootstrap/ListGroup";
+import FlipMove from "react-flip-move";
 import { filterTodoCreator, sortParamsComparator } from "./utils";
 import { Todo } from "../todo";
 
@@ -17,29 +18,31 @@ export const List = ({
   onSubTaskRemove
 }) => (
   <ListGroup>
-    {dailyTodos.length
-      ? dailyTodos
-          .filter(filterTodoCreator(activeFilter))
-          .sort(sortParamsComparator(sortingParams))
-          .map(({ id, text, complete, subTaskList, level, priority }) => (
-            <Todo
-              key={id}
-              id={id}
-              text={text}
-              complete={complete}
-              onCompleteToggle={onCompleteTodoToggle}
-              onRemove={onRemoveTodo}
-              onTodoUpdate={onTodoUpdate}
-              level={level}
-              priority={priority}
-              onLevelChange={onLevelChange}
-              onPriorityChange={onPriorityChange}
-              onSubTaskAdd={onSubTaskAdd}
-              subTaskList={subTaskList}
-              onSubTaskCompleteToggle={onSubTaskCompleteToggle}
-              onSubTaskRemove={onSubTaskRemove}
-            />
-          ))
-      : null}
+    <FlipMove>
+      {dailyTodos.length
+        ? dailyTodos
+            .filter(filterTodoCreator(activeFilter))
+            .sort(sortParamsComparator(sortingParams))
+            .map(({ id, text, complete, subTaskList, level, priority }) => (
+              <Todo
+                key={id}
+                id={id}
+                text={text}
+                complete={complete}
+                onCompleteToggle={onCompleteTodoToggle}
+                onRemove={onRemoveTodo}
+                onTodoUpdate={onTodoUpdate}
+                level={level}
+                priority={priority}
+                onLevelChange={onLevelChange}
+                onPriorityChange={onPriorityChange}
+                onSubTaskAdd={onSubTaskAdd}
+                subTaskList={subTaskList}
+                onSubTaskCompleteToggle={onSubTaskCompleteToggle}
+                onSubTaskRemove={onSubTaskRemove}
+              />
+            ))
+        : null}
+    </FlipMove>
   </ListGroup>
 );
--- app/containers/daily-todos/todo/sub-task-list.jsx
+++ app/containers/daily-todos/todo/sub-task-list.jsx
@@ -16,7 +16,7 @@ export const SubTaskList = ({
 }) => {
   return (
     <Collapse in={isOpen}>
-      <Container id='subtask-collapse'>
+      <Container id="subtask-collapse">
         <Row className="justify-content-md-center">
           <Col md={8}>
             <Input onEnter={onSubTaskInput} placeholder="input subtask" />
